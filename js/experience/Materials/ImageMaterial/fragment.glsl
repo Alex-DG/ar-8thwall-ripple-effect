f02e34b939dbe2592e5e83a4420bfe6a46181dc4
Alex-DG <alexandre.diguida@gmail.com>
2023-01-25 12:49:09 +0900

    Update fragment glsl

--- js/experience/Materials/ImageMaterial/fragment.glsl
+++ js/experience/Materials/ImageMaterial/fragment.glsl
@@ -31,28 +31,4 @@ void main() {
   vec4 color =  texture2D(uTexture, uv2);
 
   gl_FragColor = color * uOpacity;
-  // gl_FragColor = displacement;
 }
-
-// uniform sampler2D tDiffuse;
-// uniform float uTime;
-// uniform float u_progress;
-// uniform float uScale;
-// varying vec2 v_uv;
-
-// void main() {
-//   vec2 uv = v_uv;
-
-//   vec2 p = 2.0 * v_uv - 1.0; // -1 ~ 1
-//   p += 0.1 * cos(uScale * 3.7 * p.yx + 1.4 * uTime + vec2(2.2, 3.4));
-//   p += 0.1 * cos(uScale * 3.0 * p.yx + 1.0 * uTime + vec2(1.2, 3.4));
-//   p += 0.3 * cos(uScale * 5.0 * p.yx + 2.6 * uTime + vec2(4.2, 1.4));
-//   p += 0.3 * cos(uScale * 7.5 * p.yx + 3.6 * uTime + vec2(12.2, 3.4));
-
-// 	uv.x = mix(v_uv.x, length(p), u_progress);
-//   uv.y = mix(v_uv.y, 0.5 * length(p) + 0.15, u_progress);
-  
-//   vec4 color = texture2D(tDiffuse, uv);
-  
-//   gl_FragColor = color;
-//   // gl_FragColor = vec4(vec3(length(p)), 1.0);
